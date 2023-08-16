175a98da93824b22759ceaf89c24efb201d510ee
amira tilouche <ati@visto-consulting.com>
2023-08-16 12:28:29 +0100

    Your commit message

--- src/alternative-cat/alternative-cat.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { AlternativeCatService } from './alternative-cat.service';
-
-@Controller('alternative-cat')
-export class AlternativeCatController {
-  constructor(private readonly alternativeCatService: AlternativeCatService) {}
-}
--- src/alternative-cat/alternative-cat.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AlternativeCatService } from './alternative-cat.service';
-import { AlternativeCatController } from './alternative-cat.controller';
-
-@Module({
-  controllers: [AlternativeCatController],
-  providers: [AlternativeCatService]
-})
-export class AlternativeCatModule {}
